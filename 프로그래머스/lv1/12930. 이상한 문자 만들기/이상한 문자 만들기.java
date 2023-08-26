7c546bea244fba4fa9027493c769c98d9599005e
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-27 04:40:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,46 +1,46 @@
-class Solution {
-    public String solution(String s) {
-        String answer = "";
-        String[] str = s.split("");
-        
-        int index = 0;
-        for(int i=0; i<str.length; i++){
-            if(str[i].equals(" ")){
-                index = 0;
-            }
-            else if(index % 2 == 0){
-                str[i]=str[i].toUpperCase();
-                index++;
-            } else if(index % 2 != 0){
-                str[i]=str[i].toLowerCase();
-                index++;
-            }
-            answer += str[i];
-        }
-        
-        return answer;
-    }
-}
-
 // class Solution {
 //     public String solution(String s) {
 //         String answer = "";
-//         String[] strArr = s.split(" ");
+//         String[] str = s.split("");
         
-//         for(int i=0; i<strArr.length; i++){
-//             StringBuilder modifiedWord = new StringBuilder();
-//             for(int j=0; j<strArr[i].length(); j++){
-//                 char c = strArr[i].charAt(j);
-//                 if(j % 2 == 0) {
-//                     modifiedWord.append(Character.toUpperCase(c));
-//                 } else {
-//                     modifiedWord.append(Character.toLowerCase(c));
-//                 }
+//         int index = 0;
+//         for(int i=0; i<str.length; i++){
+//             if(str[i].equals(" ")){
+//                 index = 0;
 //             }
-//             strArr[i] = modifiedWord.toString();
+//             else if(index % 2 == 0){
+//                 str[i]=str[i].toUpperCase();
+//                 index++;
+//             } else if(index % 2 != 0){
+//                 str[i]=str[i].toLowerCase();
+//                 index++;
+//             }
+//             answer += str[i];
 //         }
         
-//         answer = String.join(" ", strArr);
 //         return answer;
 //     }
 // }
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] strArr = s.split(" ",-1);
+        
+        for(int i=0; i<strArr.length; i++){
+            StringBuilder modifiedWord = new StringBuilder();
+            for(int j=0; j<strArr[i].length(); j++){
+                char c = strArr[i].charAt(j);
+                if(j % 2 == 0) {
+                    modifiedWord.append(Character.toUpperCase(c));
+                } else {
+                    modifiedWord.append(Character.toLowerCase(c));
+                }
+            }
+            strArr[i] = modifiedWord.toString();
+        }
+        
+        answer = String.join(" ", strArr);
+        return answer;
+    }
+}
