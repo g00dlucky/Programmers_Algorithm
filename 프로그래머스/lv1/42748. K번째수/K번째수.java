d37641bd96d963c65e00cc7f45ddc3e6a9ffbc24
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-28 00:01:22 +0900

    [level 1] Title: K번째수, Time: 0.54 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] arr, int[][] commands) {
+        List<Integer> list = new ArrayList<>();
+        int[] intArr = {};
+        
+        
+        for(int i=0; i<commands.length; i++){
+            int[] groups = commands[i];
+            int a = groups[0]-1;
+            int b = groups[1]-1;
+            int c = groups[2]-1;
+            
+            intArr = new int[b-a+1];   
+            int index = 0;
+            for(int j=a; j<=b; j++) {
+                intArr[index++] = arr[j];
+                
+            }
+            Arrays.sort(intArr);
+            list.add(intArr[c]);
+           
+        }
+        return list;
+    }
+}
