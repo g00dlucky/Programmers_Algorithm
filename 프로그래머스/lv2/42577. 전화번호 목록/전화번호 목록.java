f176cb298fb5489c5daf99dfc0aa111616b05af4
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-28 19:15:03 +0900

    [level 2] Title: 전화번호 목록, Time: 283.23 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,10 +1,41 @@
+import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
         
         
         
         return answer;
     }
 }
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+
+// class Solution {
+//     public boolean solution(String[] phoneBook) {
+//         boolean answer = true;
+//             Map<String, Integer> map = new HashMap<>();
+//             for(int i = 0; i < phoneBook.length; i++) {
+//                 map.put(phoneBook[i], i);
+//             }
+//             for(int i = 0; i < phoneBook.length; i++) {
+//                 for(int j = 0; j < phoneBook[i].length(); j++) {
+//                     if(map.containsKey(phoneBook[i].substring(0,j))) {
+//                         answer = false;
+//                         return answer;
+//                     }
+//                 }
+//             }
+//             return answer;
+//     }
+// }
