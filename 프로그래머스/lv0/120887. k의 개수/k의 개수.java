f1e025d931af47e36b507cc45c8a82eab2461749
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-28 14:55:59 +0900

    [level 0] Title: k의 개수, Time: 14.99 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,29 +1,34 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        int index = 0;
-
+        //문자열을 이용한 방법
         for(int a=i; a<=j; a++) {
             String aStr = String.valueOf(a);
             String kStr = String.valueOf(k);
-            String[] b = aStr.split("");
-            
-            // if(aStr.contains(kStr) || (aStr.equals(kStr))) {
-            //     answer++;   
-            // }
             
             String[] parts = aStr.split(kStr, -1); 
             answer += parts.length - 1; 
-           
         }
-        
         return answer;
     }
 }
 
-            // //중복숫자처리
-            //     for(int c=0; c<b.length; c++) {
-            //         if(b[c].equals(kStr)) {
-            //             index++;
-            //         } 
-            //     }
+// //자릿수를 이용한 방법(k가 두자리수 이상일 때는 쓰기 어려움)
+// class Solution {
+//     public int solution(int i, int j, int k) {
+//         int answer = 0;
+
+//         for (int num = i; num <= j; num++){
+//             int tmp = num;
+//             while (tmp != 0){
+//                 //1의 자리 확인
+//                 if (tmp % 10 == k)
+//                     answer++;
+//                 //10의 자리 확인
+//                 //tmp가 1일 경우에는 int형식으로 인해 0이 되기 때문에 루프가 종료됨.
+//                 tmp /= 10;
+//             }
+//         }
+//         return answer;
+//     }
+// }
