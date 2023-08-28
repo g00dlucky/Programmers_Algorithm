9f5e40261dfdcfb1143632d9ba7679560004d6a7
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-28 10:38:26 +0900

    [unrated] Title: 세로 읽기, Time: 9.10 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,33 +1,38 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public String solution(String my_string, int m, int c) {
+//         String answer = "";
+//         StringBuilder sb =new StringBuilder();
+        
+        
+//         String[][] a = new String[(int) Math.ceil((double) my_string.length() / m)][m];
+//         String[] b = my_string.split("");
+//         int index = 0;
+        
+//         for(int i=0; i<a.length; i++){
+//             for(int j=0; j<m; j++) {
+//                 a[i][j] = b[index++];
+//             }
+//         }
+//         for(int k=0; k<a.length; k++) {
+//             sb.append(a[k][c-1]);
+//         }
+        
+//         answer = sb.toString();
+//         return answer;
+//     }
+// }
+
 
 class Solution {
+
     public String solution(String my_string, int m, int c) {
         String answer = "";
-        StringBuilder sb =new StringBuilder();
-        
-        
-        String[][] a = new String[(int) Math.ceil((double) my_string.length() / m)][m];
-        String[] b = my_string.split("");
-        int index = 0;
-        
-        for(int i=0; i<a.length; i++){
-            for(int j=0; j<m; j++) {
-                a[i][j] = b[index++];
-            }
+
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer += my_string.charAt(i);
         }
-        for(int k=0; k<a.length; k++) {
-            sb.append(a[k][c-1]);
-        }
-        
-        answer = sb.toString();
         return answer;
     }
 }
-
-
-//         List<String> list = new ArrayList<>();
-        
-//         for(int i=0; i<my_string.length()/m; i+=m){
-//             list.add(my_string.substring(i,m+1));
-//         }
-//         answer = list.get(c-1);
