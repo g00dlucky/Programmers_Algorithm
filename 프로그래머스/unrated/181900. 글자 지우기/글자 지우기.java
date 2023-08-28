8cca5a4f1a7744ea2c6568a13782eb8661812eb1
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-28 15:59:58 +0900

    [unrated] Title: 글자 지우기, Time: 1.89 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -3,24 +3,45 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        List<String> list = new ArrayList<>();
         String[] strArr = my_string.split("");
-        boolean[] isPresent = new boolean[my_string.length()];
         
-        for(int x : indices) {
-           isPresent[x] = true;
+        for(int i=0; i<indices.length; i++) {
+            strArr[indices[i]] = "";
         }
         
-        for(int i=0; i<strArr.length; i++) {
-            if(!isPresent[i]) {
-                list.add(strArr[i]);
-            }
-        }
-        
-        for(String x : list) {
+        for(String x : strArr) {
             answer += x;
         }
-
+        
         return answer;
     }
 }
+
+
+//boolean을 이용하는 방법
+// import java.util.*;
+
+// class Solution {
+//     public String solution(String my_string, int[] indices) {
+//         String answer = "";
+//         List<String> list = new ArrayList<>();
+//         String[] strArr = my_string.split("");
+//         boolean[] isPresent = new boolean[my_string.length()];
+        
+//         for(int x : indices) {
+//            isPresent[x] = true;
+//         }
+        
+//         for(int i=0; i<strArr.length; i++) {
+//             if(!isPresent[i]) {
+//                 list.add(strArr[i]);
+//             }
+//         }
+        
+//         for(String x : list) {
+//             answer += x;
+//         }
+
+//         return answer;
+//     }
+// }
