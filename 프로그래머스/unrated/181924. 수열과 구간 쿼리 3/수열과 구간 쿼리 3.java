111f6b6ea3cd69427361462b0fd372519a2e9b2e
g00dlucky <131109836+g00dlucky@users.noreply.github.com>
2023-08-28 13:10:57 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 1.34 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -6,6 +6,8 @@ class Solution {
         int[] answer2 = {};
         for(int i=0; i<queries.length; i++){
             int[] group = queries[i];
+            //answer배열의 현재상태를 그대로 복사해오는 배열을 하나 더 선언.
+            //동일 상태의 index의 value를 가져오기 위해서.
             answer2= Arrays.copyOfRange(answer,0,answer.length);
             answer[group[0]] = answer2[group[1]];
             answer[group[1]] = answer2[group[0]];
@@ -13,3 +15,22 @@ class Solution {
         return answer;
     }
 }
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public int[] solution(int[] arr, int[][] queries) {
+//         int[] answer = Arrays.copyOf(arr, arr.length);
+
+//         for (int[] query : queries) {
+//             int i = query[0];
+//             int j = query[1];
+
+//             int temp = answer[i];
+//             answer[i] = answer[j];
+//             answer[j] = temp;
+//         }
+
+//         return answer;
+//     }
+// }
